895741b7f7bb9a77708026305ae2decdf2674ed0
fathaladdin99 <fathaladdin99@gmail.com>
2025-07-10 11:54:17 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'abx-calc-cache-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+  );
+});
